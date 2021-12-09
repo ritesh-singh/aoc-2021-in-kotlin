af4876b577ca3c5bcc4dd5847ad7ac94bc8d09c1
Ritesh <riteshdvps10@gmail.com>
2021-12-09 22:12:31 +0530

    Day 8 and 9

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -77,16 +77,89 @@ fun main(){
         return totalCount
     }
 
-    fun part2(inputs: List<String>): Int {
-        return inputs.size
+    // Required to check if it contains any location in bing
+    val allLocationsInBasin = mutableListOf<Pair<Int, Int>>()
+
+    data class BasinCounter(var count: Int = 0)
+
+    // Locations of height 9 do not count as being in any basin
+    // all other locations will always be part of exactly one basin (row wise)
+    fun getBasin(matrix: Array<IntArray>, lowPoint: Pair<Int, Int>, counter: BasinCounter) {
+        val rowSize = matrix.size
+        val colSize = matrix[0].size
+
+        for (direction in Direction.values()) {
+            val currentLocation = matrix[lowPoint.first][lowPoint.second]
+            val locFromDirection = getLocationBasedOnDirection(lowPoint.first,lowPoint.second, direction)
+            val isLocValid = isLocationValid(locFromDirection.first,locFromDirection.second,rowSize, colSize)
+            if (
+                isLocValid
+                && matrix[locFromDirection.first][locFromDirection.second] > currentLocation
+                && matrix[locFromDirection.first][locFromDirection.second] != 9
+                && !allLocationsInBasin.contains(locFromDirection)
+            ){
+                allLocationsInBasin.add(locFromDirection)
+                counter.count = counter.count + 1
+                getBasin(matrix,locFromDirection,counter)
+            }
+        }
     }
 
+    fun part2(inputs: List<String>): Int {
+        val rowSize = inputs.size
+        val colSize = inputs[0].length
+
+        val matrix = Array(rowSize) { IntArray(colSize) }
+        inputs.initMatrix(matrix = matrix)
+
+        val lowestPoints = mutableListOf<Pair<Int, Int>>()
+
+        // find lowest points
+        for (row in 0 until rowSize) {
+            for (col in 0 until colSize) {
+                val current = matrix[row][col]
+                var isLowest = false
+                for (direction in Direction.values()){
+                    val pair = getLocationBasedOnDirection(row, col, direction)
+                    if (isLocationValid(pair.first, pair.second,rowSize, colSize)) {
+                        if (current <  matrix[pair.first][pair.second]){
+                            isLowest = true
+                        }else{
+                            isLowest = false
+                            break
+                        }
+                    }
+                }
+                if (isLowest) {
+                    lowestPoints.add(Pair(row,col))
+                }
+            }
+        }
+
+
+        val listOfBasins = mutableListOf<Int>()
+        // for each low point find basin
+        for (lowPoint in lowestPoints) {
+            allLocationsInBasin.add(Pair(lowPoint.first, lowPoint.second))
+            // every low point has a basin
+            // The size of a basin is the number of locations within the basin,
+            // including the low point. (count = 1) -> includes low point count
+            val counter = BasinCounter()
+            counter.count = 1
+            getBasin(matrix = matrix, lowPoint = lowPoint, counter = counter)
+            listOfBasins.add(counter.count)
+        }
+
+        listOfBasins.sortDescending()
+
+        return listOfBasins.take(3).reduce { acc, i -> acc * i }
+    }
 
     val testInput = readInput("/day09/Day09_test")
     check(part1(testInput) == 15)
-//    check(part2(testInput) == 1134)
+    check(part2(testInput) == 1134)
 
     val input = readInput("/day09/Day09")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
